15700d4e9eea6ef6fd60e80cf99897e68d045893
dsbert <danbertolini@gmail.com>
2016-09-27 14:48:22 -0400

    updates

--- transmission-vpn/transmission-vpn.sh
+++ transmission-vpn/transmission-vpn.sh
@@ -1,18 +1,23 @@
 # install docker compose
-sudo curl -L https://github.com/hypriot/compose/releases/download/1.2.0-raspbian/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-or
-
-Actually I may have found a quicker way to install it:
 sudo apt-get -y install python-pip
 sudo pip install docker-compose
 
 # get docker container
-git https://github.com/haugene/docker-transmission-openvpn ~/docker-transmission-openvpn
-# set variables (data path, vpn info, local net) or copy file
-todo
-# run container - why is this failing?
+git clone https://github.com/haugene/docker-transmission-openvpn ~/docker-transmission-openvpn
+
+# set variables (data path, vpn info, local net) by copying docker-compose file
+sudo mkdir -p /mount/rpconfig
+sudo mount blackbox:/volume1/transfer/rpconfig /mount/rpconfig
+cp /mount/rpconfig/docker-compose-armhf.yml ~/docker-transmission-openvpn/docker-compose-armhf.yml
+cp /mount/rpconfig/nfs-watch.sh /usr/local/bin/nfs-watch.sh
+
+# run container
 cd ~/docker-transmission-openvpn
 docker-compose --file docker-compose-armhf.yml up -d
+
 # set up touch script to initiate file change in watch folder
-todo
+chmod u+x /usr/local/bin/nfs-watch.sh
+sudo echo "* * * * * root sh /usr/local/bin/nfs-watch.sh" >> /etc/crontab
+
+# clean up
+#todo
